8440c05c382d821c2a189f4aba72a47fdccff264
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-02 20:24:55 +0300

    server

--- app.js
+++ app.js
@@ -5,9 +5,53 @@ const conectionDB = require('../server/DB/db')
 const cors = require('cors')
 const helmet = require('helmet')
 const xss = require('xss-clean')
-
+const cookie = require('cookie-parser')
+const rateLimit = require('express-rate-limit')
+require('dotenv').config()
 
 
 
 //SECURITY MIDDLEWARES  
+
+app.use(cors())
+app.use(helmet())
+app.use(xss())
+app.use(cookie())
+app.use(express.json())
+app.set('trust proxy', 1)
+app.use(rateLimit({
+    windowsMs:15*60*100,
+    max:100
+}))
+
+
+
+//ROUTES
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONNECTION TO THE DB
+
+
+
+
 
