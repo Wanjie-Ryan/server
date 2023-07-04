b81ae55ce504a1ca22fa7721c120b7ce43635e5e
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-04 12:36:46 +0300

    server

---
+++ Controllers/Aspirants/GetVoters.js
@@ -0,0 +1,17 @@
+const votersmodel = require('../../Models/voters')
+const {StatusCodes} = require('http-status-codes')
+const error = require('../../utils/error');
+
+
+
+
+const AllVoters = (req, res)=>{
+
+
+
+
+
+}
+
+
+module.exports ={AllVoters}
