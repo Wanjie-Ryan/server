00abb18bf5a850818fe92e522e15815f6acc71f1
Ryan <ryanwanjiedistortion@gmail.com>
2023-07-04 09:29:07 +0300

    server

--- Models/voters.js
+++ Models/voters.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 
+
+
 const VotersSchema = mongoose.Schema({
 
     AspirantID:{
@@ -9,6 +11,25 @@ const VotersSchema = mongoose.Schema({
     },
 
     name:{
-        
+
+        type:String,
+        required:[true, 'The voters name is required']
+
+    },
+
+    email:{
+
+        type:String,
+        required:[true, 'The voters email is required'],
+        unique:true,
+        match:[
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email',
+            
+            ],
     }
-})
+
+},{timestamps:true})
+
+
+
+module.exports = mongoose.model('Voters',VotersSchema )
